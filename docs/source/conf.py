7f2ea706a0ab9c3b7132a595067f948d94b5635f
SHIHONGHAO <13820618441@163.com>
2022-04-19 13:54:56 +0800

    change author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LibEpidemic'
+copyright = '2022, Beihang Uni'
+author = 'Shi Honghao'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
